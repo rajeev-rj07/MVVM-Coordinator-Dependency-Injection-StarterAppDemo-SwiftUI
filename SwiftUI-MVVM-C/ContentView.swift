3fa132aa83e2b767b63825b6d2cdef22f0ea9f9c
Rajeev Kulariya <dev.rajeev1996@gmail.com>
2024-12-17 20:46:53 +0530

    First Comment

---
+++ SwiftUI-MVVM-C/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-MVVM-C
+//
+//  Created by Rajeev Kulariya on 17/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-MVVM-C/SwiftUI_MVVM_CApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVM_CApp.swift
+//  SwiftUI-MVVM-C
+//
+//  Created by Rajeev Kulariya on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVM_CApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
